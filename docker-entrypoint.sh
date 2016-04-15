e9168e5e90290f0c24cf076ef5a6b9800797fead
Jorrit Salverda <jsalverda@travix.com>
2016-04-15 09:12:14 +0200

    make sure agent starts with read-only mounted ssh and gcloud directories

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,22 +28,22 @@ fi
 
 if [ -d "/var/go/.ssh" ];
 then
-  chown -R ${USER_NAME}:${GROUP_NAME} /var/go/.ssh;
+  chown -R ${USER_NAME}:${GROUP_NAME} /var/go/.ssh || echo "No write permissions for /var/go/.ssh";
 
   # make sure ssh keys mounted from kubernetes secret have correct permissions
-  chmod 400 /var/go/.ssh/*;
+  chmod 400 /var/go/.ssh/* || echo "No write permissions for /var/go/.ssh";
 
   # rename ssh keys to deal with kubernetes secret name restrictions
   cd /var/go/.ssh;
   for f in *-*;
-    do mv "$f" "${f//-/_}";
+    do mv "$f" "${f//-/_}" || echo "No write permissions for /var/go/.ssh";
   done;
 
 fi
 
 if [ -d "/var/go/.gcloud" ];
 then
-  chown -R ${USER_NAME}:${GROUP_NAME} /var/go/.gcloud;
+  chown -R ${USER_NAME}:${GROUP_NAME} /var/go/.gcloud || echo "No write permissions for /var/go/.ssh";
 fi
 
 # update config to point to correct go.cd server hostname and port
