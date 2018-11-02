05b4241656badea475f51d29dca5221e7f5c2b56
Jorrit Salverda <jsalverda@travix.com>
2018-11-02 11:18:00 +0100

    increase concurrent downloads for docker pull to 10; use mirror.gcr.io

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -59,7 +59,7 @@ done
 # run dockerd
 if [ "${RUN_DOCKER_DAEMON}" == "true" ]; then
   echo "Starting docker daemon..."
-  dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --storage-driver=$STORAGE_DRIVER &
+  dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --storage-driver=$STORAGE_DRIVER --max-concurrent-downloads=10 --registry-mirror=https://mirror.gcr.io &
 fi
 
 # run go.cd agent
